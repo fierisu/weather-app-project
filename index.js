de5dab8c9665d515d2ddc335bc3bd3820d84b326
Alina <linakotovva@gmail.com>
2023-02-02 15:31:47 +0200

    cleared code

--- index.js
+++ index.js
@@ -1,60 +1,12 @@
 const searchButton = document.querySelector("#search-text-input");
-const header = document.querySelector("header");
+const header = document.querySelector("#city-element");
 const searchFor = document.querySelector(".input-city");
-const temperatureCF = document.querySelector(".temperature");
+const temperature = document.querySelector("#temperature-element");
+const temperatureCelsius = document.querySelector(".to-celsius");
+const temperatureFahrenheit = document.querySelector("#to-fahrenheit");
 const apiKey = "9b2899fa2589bca94665fbb84db2ef36";
 const currentData = document.querySelector(".current-location-button");
 const showCurrentDataBtn = document.querySelector('.show-current-data');
-
-showCurrentDataBtn.addEventListener('click', showCurrentData)
-
-function showData (city) {
-  let apiUrlSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9b2899fa2589bca94665fbb84db2ef36`;
-  axios.get(`${apiUrlSearch}&appid=${apiKey}`).then(showCityData);
-}
-
-function showCityData (response) {
- header.innerHTML = `${response.data.name}`
- temperatureCF.innerHTML = `${Math.round(response.data.main.temp)}°C`
-}
-
-function showCity(event) {
-  console.log(event);
-  event.preventDefault();
-  if (searchFor.value) {
-    showData(searchFor.value)
-  } else {
-    header.innerHTML = null;
-    alert("Please, enter a city");
-  }
-}
-
-searchButton.addEventListener("click", showCity);
-
-function showCurrentData () {
-  function handlePosition(position) {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9b2899fa2589bca94665fbb84db2ef36`;
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(res => {
-      showCurrentCity(res)
-      showTemperature(res)
-    });
-  }
-  
-    navigator.geolocation.getCurrentPosition(handlePosition);
-  
-  function showTemperature (response) {
-    let temperatureCelcius = Math.round(response.data.main.temp);
-    let tempSection = document.querySelector(".temperature");
-    tempSection.innerHTML = `${temperatureCelcius}°C`;
-   }
-  
-   function showCurrentCity (response) {
-    let currentCity = response.data.name;
-    header.innerHTML = `${currentCity}`;
-   }
-}
-
-let temp = 21;
 let now = new Date();
 let time = document.querySelector(".time");
 let days = [
@@ -79,6 +31,59 @@ if (hours < 10) {
 
 time.innerHTML = `${day} ${hours}:${minutes}`;
 
+navigator.geolocation.getCurrentPosition(handlePosition);
+
+showCurrentDataBtn.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+})
+
+searchButton.addEventListener("click", showCity);
+
+function handlePosition(position) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9b2899fa2589bca94665fbb84db2ef36`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(res => {
+    showCurrentCity(res)
+    showTemperature(res)
+  });
+}
+
+
+function showData (city) {
+  let apiUrlSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9b2899fa2589bca94665fbb84db2ef36`;
+  axios.get(`${apiUrlSearch}&appid=${apiKey}`).then(showCityData);
+}
+
+function showCityData (response) {
+ header.innerHTML = `${response.data.name}`
+ temperature.innerHTML = `${Math.round(response.data.main.temp)}°C`
+}
+
+function showCity(event) {
+  event.preventDefault();
+  if (searchFor.value) {
+    showData(searchFor.value)
+  } else {
+    header.innerHTML = null;
+    alert("Please, enter a city");
+  }
+}
+
+
+function showTemperature (response) {
+  let temperatureCelcius = Math.round(response.data.main.temp);
+  let tempSection = document.querySelector(".temperature");
+  tempSection.innerHTML = `${temperatureCelcius}`;
+ }
+
+ function showCurrentCity (response) {
+  let currentCity = response.data.name;
+  header.innerHTML = `${currentCity}`;
+ }
+
+ function toCelsius (event) {
+  temperatureCelsius = Math.round((temp * 9) / 5 + 32);
+ }
+
 function toFahrenheit(event) {
   if (temperatureCF.classList.contains("celsium")) {
     temp = Math.round((temp * 9) / 5 + 32);
@@ -93,4 +98,3 @@ function toFahrenheit(event) {
   }
 }
 
-temperatureCF.addEventListener("click", toFahrenheit);
